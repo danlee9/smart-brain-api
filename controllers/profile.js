c0663485d6a2f7357db99aa7f2a780288fb60707
Daniel Lee <dan.dh.lee@gmail.com>
2020-03-28 19:20:39 -0700

    Add profile update route

--- controllers/profile.js
+++ controllers/profile.js
@@ -11,6 +11,24 @@ const handleProfileGet = (req, res, db) => {
     .catch(err => res.status(400).json('error getting user'))
 }
 
+const handleProfileUpdate = (req, res, db) => {
+  const { id } = req.params;
+  // should also add some checking here for security but do it later
+  const { name, age, pet } = req.body.formInput;
+  db('users')
+    .where({ id })
+    .update({ name }) // don't have age and pet in db yet
+    .then(resp => {
+      if (resp) {
+        res.json('success');
+      } else {
+        res.status(400).json('Unable to update');
+      }
+    })
+    .catch(err => res.status(400).json('error updating user'));
+}
+
 module.exports = {
-  handleProfileGet
+  handleProfileGet,
+  handleProfileUpdate
 }
