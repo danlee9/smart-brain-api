b4c5a8bfed2946aaed8b5f02c02849c8df930ec6
Daniel Lee <dan.dh.lee@gmail.com>
2020-04-03 00:24:22 -0700

    Add initial jwt code

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 // const handleSignin = (db, bcrypt) => (req, res) => {
 //   const { email, password } = req.body;
 //   if (!email || !password) {
@@ -46,10 +48,22 @@ const getAuthTokenId = () => {
   console.log('auth ok');
 }
 
+const signToken = email => {
+  const jwtPayload = { email };
+  return jwt.sign(jwtPayload, 'JWT_SECRET');
+}
+
+const createSessions = user => {
+  const { email, id } = user;
+  const token = signToken(email);
+}
+
 const signinAuthentication = (db, bcrypt) => (req, res) => {
   const { authorization } = req.headers;
   return authorization ? getAuthTokenId() : handleSignin(db, bcrypt, req, res)
-    .then(data => res.json(data))
+    .then(data => {
+      data.id && data.email ? createSessions(data) : Promise.reject(data);
+    })
     .catch(err => res.status(400).json(err));
 }
 
