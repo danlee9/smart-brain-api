2ca58f7de395bef009f60af04a5b815a4766fecc
Daniel Lee <dan.dh.lee@gmail.com>
2020-04-08 00:56:02 -0700

    Use redis to set tokens

--- controllers/signin.js
+++ controllers/signin.js
@@ -56,10 +56,18 @@ const signToken = email => {
   return jwt.sign(jwtPayload, 'JWT_SECRET', { expiresIn: '2 days' });
 }
 
+const setToken = (token, id) => {
+  return Promise.resolve(redisClient.set(token, id))
+}
+
 const createSessions = user => {
   const { email, id } = user;
   const token = signToken(email);
-  return { success: 'true', userId: id, token };
+  return setToken(token, id)
+    .then(() => {
+      return { success: 'true', userId: id, token }
+    })
+    .catch(console.log);
 }
 
 const signinAuthentication = (db, bcrypt) => (req, res) => {
